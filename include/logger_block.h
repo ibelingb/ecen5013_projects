3127bc9fb05baaa91280079dd1e9e54b4c4eab24
shua <joshua.malburg@colorado.edu>
2019-03-31 23:35:06 -0400

    more comment updates

--- include/logger_block.h
+++ include/logger_block.h
@@ -21,8 +21,27 @@
 
 #include "logger_types.h"
 
+/**
+ * @brief  init logger
+ * 
+ * @param pArg stuff it needs
+ * @return uint8_t 
+ */
 uint8_t init_logger_block(void * pArg);
+
+/**
+ * @brief put an item in logger
+ * 
+ * @param pLogItem item to enter
+ * @return uint8_t success of operation
+ */
 uint8_t log_item(logItem_t *pLogItem);
+
+/**
+ * @brief flush the logger
+ * 
+ * @return uint8_t successof operation
+ */
 uint8_t log_flush(void);
 
 #endif	/* LOGGER_BLOCK_H */
