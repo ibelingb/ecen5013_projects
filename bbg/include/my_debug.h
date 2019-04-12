032040a12810947022712a54f808ab9ef0d3ee0b
Joshua Malburg <malburgj@gdls.com>
2019-04-12 18:21:03 -0400

    finished switch to debug.h for prints

--- bbg/include/my_debug.h
+++ bbg/include/my_debug.h
@@ -17,9 +17,11 @@
 #ifndef DEBUG_H_
 #define DEBUG_H_
 
-#include <stdio.h>
-#include <string.h>
 #include <errno.h>
+#include <string.h>
+
+#ifdef __linux__
+#include <stdio.h>
 
 #define ERROR_PRINT(...)	(printf(__VA_ARGS__))
 #define ERRNO_PRINT(str)    (ERROR_PRINT("%s, error in: %s, err#%d (%s)\n", str, __func__, errno, strerror(errno)))
@@ -27,5 +29,13 @@
 #define INFO_PRINT(...)		(printf(__VA_ARGS__))
 #define MUTED_PRINT(...)	
 #define DEBUG_PRINT(...)	//(printf(__VA_ARGS__))
-
+#else
+#include "uartstdio.h"
+#define ERROR_PRINT(...)    (UARTprintf(__VA_ARGS__))
+#define ERRNO_PRINT(str)    (ERROR_PRINT("%s, error in: %s, err#%d (%s)\n", str, __func__, errno, strerror(errno)))
+#define WARN_PRINT(...)     (UARTprintf(__VA_ARGS__))
+#define INFO_PRINT(...)     (UARTprintf(__VA_ARGS__))
+#define MUTED_PRINT(...)
+#define DEBUG_PRINT(...)    (UARTprintf(__VA_ARGS__))
+#endif
 #endif /* DEBUG_H_ */
