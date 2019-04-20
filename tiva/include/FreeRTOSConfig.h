ff5b86eff7e38984a0622ad623de8bc45d45bcff
Joshua Malburg <malburgj@gdls.com>
2019-04-19 21:11:21 -0400

    finished upgrade to FreeRTOS 10.2.0

--- tiva/include/FreeRTOSConfig.h
+++ tiva/include/FreeRTOSConfig.h
@@ -88,24 +88,26 @@
 #define configUSE_TICKLESS_IDLE         0
 #define configTICK_RATE_HZ              ( ( TickType_t ) 1000 )
 
-#define configUSE_PREEMPTION            1
-#define configUSE_IDLE_HOOK             0
-#define configUSE_TICK_HOOK             0
-#define configMAX_PRIORITIES            ( 5 )
-#define configMINIMAL_STACK_SIZE        ( ( unsigned short ) 200 )
-#define configTOTAL_HEAP_SIZE           ( ( size_t ) ( 50000 ) )
-#define configMAX_TASK_NAME_LEN         ( 10 )
-#define configUSE_TRACE_FACILITY        1
-#define configUSE_16_BIT_TICKS          0
-#define configIDLE_SHOULD_YIELD         1
-#define configUSE_MUTEXES               1
-#define configQUEUE_REGISTRY_SIZE       8
-#define configCHECK_FOR_STACK_OVERFLOW  0
-#define configUSE_RECURSIVE_MUTEXES     1
-#define configUSE_MALLOC_FAILED_HOOK    0
-#define configUSE_APPLICATION_TASK_TAG  0
-#define configUSE_COUNTING_SEMAPHORES   1
-#define configUSE_TASK_NOTIFICATIONS    1
+#define configUSE_PREEMPTION                1
+#define configUSE_IDLE_HOOK                 0
+#define configUSE_TICK_HOOK                 0
+#define configMAX_PRIORITIES                ( 5 )
+#define configMINIMAL_STACK_SIZE            ( ( unsigned short ) 200 )
+#define configTOTAL_HEAP_SIZE               ( ( size_t ) ( 50000 ) )
+#define configMAX_TASK_NAME_LEN             ( 10 )
+#define configUSE_TRACE_FACILITY            1
+#define configUSE_16_BIT_TICKS              0
+#define configIDLE_SHOULD_YIELD             1
+#define configUSE_MUTEXES                   1
+#define configQUEUE_REGISTRY_SIZE           8
+#define configCHECK_FOR_STACK_OVERFLOW      0
+#define configUSE_RECURSIVE_MUTEXES         1
+#define configUSE_MALLOC_FAILED_HOOK        0
+#define configUSE_APPLICATION_TASK_TAG      0
+#define configUSE_COUNTING_SEMAPHORES       1
+#define configUSE_TASK_NOTIFICATIONS        1
+#define configSUPPORT_DYNAMIC_ALLOCATION    1
+#define configSUPPORT_STATIC_ALLOCATION     0
 
 /* Co-routine definitions. */
 #define configUSE_CO_ROUTINES           0
